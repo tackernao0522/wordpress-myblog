ab5deb45e7200efaed2d5965475c59395a9cc87e
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2020-11-15 12:47:31 +0900

    投稿のページ(single.php)の仕上げ

--- single.php
+++ single.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 
 <head>
 
@@ -7,17 +7,17 @@
 
 </head>
 
-<body>
+<body <?php body_class(); ?>>
 
   <?php get_template_part('includes/header'); ?>
   <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
       <!-- Page Header -->
       <?php
-      if (has_post_thumbnail()):
+      if (has_post_thumbnail()) :
         $id = get_post_thumbnail_id();
         $img = wp_get_attachment_image_src($id, 'large');
-      else:
+      else :
         $img = array(get_template_directory_uri() . '/img/post-bg.jpg');
       endif;
       ?>
